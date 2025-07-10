3faded491c5c9ed4aa73abaeafd77725b2679281
Sibiya07 <134922789+Sibiya07@users.noreply.github.com>
2025-07-10 20:03:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,30 +12,19 @@ const app = express();
 app.use(cors({
   origin: ['http://localhost:3000', 'https://skillswap-client-7d45.onrender.com'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
+app.use(express.json());
 
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers'] || '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    return res.sendStatus(204);
-  }
-  next();
-});
 app.get('/api/test', (req, res) => {
   res.send('Backend is working!');
 });
 
-app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/exchange', exchangeRoutes);
 app.use('/uploads', express.static('uploads'));
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(process.env.PORT, () => console.log("Server running")))
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`)))
   .catch(err => console.log(err));
